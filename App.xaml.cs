8b6a85c34e461e5a56ac165f4cf112bcde369ff0
iiixxov <iiixxov@gmail.com>
2023-04-28 03:30:07 +0700

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SwordDamage_WPF_Part_2
+{
+
+    public partial class App : Application
+    {
+    }
+}
